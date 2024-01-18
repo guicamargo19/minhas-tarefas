54b9f49e56b673b8c5d6fc2117280a4e2f0c9ce4
camargo <gui.ferreira.camargo@gmail.com>
2024-01-18 20:08:11 -0300

    Finalizado criação dos componentes e estilização da página completa

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 import { Props } from '.'
 
-export const Card = styled.div<Props>`
+type PropsSemLegendaEContador = Omit<Props, 'contador' | 'legenda'>
+
+export const Card = styled.div<PropsSemLegendaEContador>`
   padding: 8px;
   border: 1px solid ${(props) => (props.ativo ? '#1e90ff' : '#a1a1a1')};
-  background-color: #fcfcfc;
+  background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
   color: ${(props) => (props.ativo ? '#1e90ff' : '#5e5e5e')};
   border-radius: 8px;
 `
---
+++ src/components/Tarefa/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './styles'
+
+const Tarefa = () => (
+  <>
+    <S.Card>
+      <S.Titulo>Nome Tarefa</S.Titulo>
+      <S.Tag>importante</S.Tag>
+      <S.Tag>pendente</S.Tag>
+      <S.Descricao />
+      <S.BarraAcoes>
+        <S.Botao>Editar</S.Botao>
+        <S.Botao>Remover</S.Botao>
+      </S.BarraAcoes>
+    </S.Card>
+  </>
+)
+
+export default Tarefa
